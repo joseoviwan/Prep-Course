496f2da065cfeb267a63dea2c452d42c2502ae04
joseoviwan <121641426+joseoviwan@users.noreply.github.com>
2023-01-17 23:09:58 -0300

    actualizacion 2.0

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -38,10 +38,10 @@ function suma (x, y) {
   return x + y;
 }
 
-function resta(x, y) {
+function resta (x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return y-x;
+  return y - x;
 
 }
 
@@ -51,10 +51,10 @@ function multiplica(x, y) {
   return x * y;
 }
 
-function divide(x, y) {
+function divide (x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return x / y;
+  return x/y;
 }
 
 function sonIguales(x, y) {
@@ -83,34 +83,44 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-}
+  if (num < 90) {return true;
+  } else {
+    return false;
+  }
+  }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num >50) {return true;
+  } else {return false;
+}
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  return x % y; 
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ((num % 2) === 0) {return true;
+  } else {return false;
+  }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+}
+if((num % 2) !== 0)  {
+  return true
+ 
 }
 
 function elevarAlCuadrado(num) {
